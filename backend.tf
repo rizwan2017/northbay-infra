078b7ec7e4ffa5c74f6a03cc820800ca7f193ae1
Rizwan Nadeem <rizwan2017@gmail.com>
2025-07-17 04:43:14 +0500

    added module files

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatenorthbay"
-    container_name       = "tfstate-northbay"
-    key                  = "terraform.tfstate"
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "tfstate-rg"
+#     storage_account_name = "tfstatenorthbay"
+#     container_name       = "tfstate-northbay"
+#     key                  = "terraform.tfstate"
+#   }
+# }
